cbf5ddff3df4db0398676a6c9f59a9fb5f24e046
Andrew Lawson <alawson@jwplayer.com>
2017-07-12 00:36:25 -0400

    Set up webpack for less modules. Add globals and variables files. Add material-ui.

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import * as injectTapEventPlugin from "react-tap-event-plugin";
+
 import Main from "./components/main";
+
+const styles = require('./styles/global.less');
+
+injectTapEventPlugin();
 
 class App extends React.Component<any, any> {
   render() {
     return (
-      <Main/>
+      <MuiThemeProvider>
+        <Main/>
+      </MuiThemeProvider>
     )
   }
 }
