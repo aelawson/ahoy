a96d79b1b5d981d683aabdfccdd254ca41ba944f
Andrew Lawson <alawson@jwplayer.com>
2017-07-16 17:50:49 -0400

    Update content module to use new repostories module. Use material components. Add new styles.

--- app/components/release/content.tsx
+++ app/components/release/content.tsx
@@ -1,13 +1,22 @@
 import * as React from "react";
+import {Card, CardTitle, CardText, CardHeader} from 'material-ui/Card';
+
+import Repositories from './repositories';
 
 const styles = require("./content.module.less");
 
 class ReleaseContent extends React.Component<any, any> {
   render() {
     return (
-      <div className={styles.container}>
-        <h1>My content</h1>
-      </div>
+      <Card className="section">
+        <CardTitle
+          title="Release Plan"
+          subtitle="Configure release and execute action items"
+        />
+        <CardText>
+          <Repositories/>
+        </CardText>
+      </Card>
     )
   }
 }
