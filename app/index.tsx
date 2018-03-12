ddbebc5552f26f0c8ff1c13f047782324ae6d0ce
Andrew Lawson <alawson@jwplayer.com>
2018-03-11 20:25:11 -0400

    Integrate react redux.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import * as ReactDOM from "react-dom";
 
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
@@ -6,21 +8,26 @@ import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import * as injectTapEventPlugin from "react-tap-event-plugin";
 
 import Main from "./components/main";
+import reducer from './reducers';
+
 const styles = require('./styles/global.less');
+const store = createStore(reducer);
 
 injectTapEventPlugin();
 
 class App extends React.Component<any, any> {
   render() {
     return (
-      <MuiThemeProvider>
-        <Main/>
-      </MuiThemeProvider>
+      <Provider store={store}>
+        <MuiThemeProvider>
+          <Main/>
+        </MuiThemeProvider>
+      </Provider>
     )
   }
 }
 
 ReactDOM.render(
   <App/>,
-  document.getElementById('app')
+  document.getElementById('app') as HTMLElement
 );
