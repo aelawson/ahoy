7ea1e321524be4c7697d5e9a2c92c45ae3b7f913
Andrew Lawson <alawson@jwplayer.com>
2018-03-17 17:25:59 -0400

    Define team actions. Fix interface definition.

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,8 +1,11 @@
 export default interface Action<T> {
   type: string;
   payload?: T;
-  error?: boolean,
-  meta?: any
+  error?: boolean;
+  meta?: any;
 };
 
 export const TEAMS_FETCH = 'TEAMS_FETCH';
+export const TEAMS_REQUEST = 'TEAMS_REQUEST';
+export const TEAMS_RECEIVE = 'TEAMS_RECEIVE';
+export const TEAMS_SELECT = 'TEAMS_SELECT';
