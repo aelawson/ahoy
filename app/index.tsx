9c68d112c37e8bc49358db2583dcfdd6c2fa27ef
Andrew Lawson <alawson@jwplayer.com>
2018-03-17 18:00:46 -0400

    Move team reducers to their own module.

--- app/index.tsx
+++ app/index.tsx
@@ -7,13 +7,14 @@ import { createLogger } from 'redux-logger';
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 import Main from "./components/main";
-import reducer from './reducers';
+import rootReducer from './reducers';
 import { selectTeam, fetchTeam } from './actions/teams';
 
-const loggerMiddleware = createLogger()
+const loggerMiddleware = createLogger();
+
 const styles = require('./styles/global.less');
 const store = createStore(
-  reducer,
+  rootReducer,
   applyMiddleware(
     thunkMiddleware,
     loggerMiddleware
--- app/reducers/index.ts
+++ app/reducers/index.ts
@@ -1,60 +1,8 @@
 import { combineReducers } from 'redux';
-import Action from '../actions';
-import TeamPayload from '../actions/teams';
-
-import {
-  TEAMS_SELECT,
-  TEAMS_FETCH,
-  TEAMS_REQUEST,
-  TEAMS_RECEIVE
-} from '../actions';
-
-function teamIdReducer(state: number=null, action: Action<TeamPayload>) {
-  switch (action.type) {
-    case TEAMS_SELECT:
-      return action.payload.team;
-    default:
-      return state;
-  }
-}
-
-function teamsReducer(state: object={}, action: Action<TeamPayload>) {
-  switch (action.type) {
-    case TEAMS_REQUEST:
-      return Object.assign({}, state, {
-        meta: {
-          isFetching: true,
-          requestedAt: action.meta.requestedAt
-        }
-      });
-    case TEAMS_RECEIVE:
-      return Object.assign({}, state, {
-        data: action.payload.data,
-        meta: {
-          isFetching: false,
-          receivedAt: action.meta.receivedAt
-        },
-      });
-    default:
-      return state;
-  }
-}
-
-function dataByTeamReducer(state: any={}, action: Action<TeamPayload>) {
-  switch(action.type) {
-    case TEAMS_REQUEST:
-    case TEAMS_RECEIVE:
-      return Object.assign({}, state, {
-        [action.payload.team]: teamsReducer(state[action.payload.team], action)
-      });
-    default:
-      return state;
-  }
-}
+import teamsReducer from './teams';
 
 const rootReducer = combineReducers({
-  teamId: teamIdReducer,
-  teams: dataByTeamReducer
-})
+  teams: teamsReducer
+});
 
 export default rootReducer;
