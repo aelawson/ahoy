82473acfa2d92eae33ca4a3c88f4077e473b4b48
Andrew Lawson <alawson@jwplayer.com>
2018-03-31 14:54:07 -0400

    Split team actions into team by id and teams.

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -6,6 +6,8 @@ export default interface Action<T> {
 };
 
 export const TEAMS_FETCH = 'TEAMS_FETCH';
+export const TEAM_REQUEST = 'TEAM_REQUEST';
 export const TEAMS_REQUEST = 'TEAMS_REQUEST';
+export const TEAM_RECEIVE = 'TEAM_RECEIVE';
 export const TEAMS_RECEIVE = 'TEAMS_RECEIVE';
 export const TEAMS_SELECT = 'TEAMS_SELECT';
--- app/actions/teams.ts
+++ app/actions/teams.ts
@@ -6,26 +6,28 @@ import {
   TEAMS_SELECT,
   TEAMS_FETCH,
   TEAMS_REQUEST,
-  TEAMS_RECEIVE
+  TEAM_REQUEST,
+  TEAMS_RECEIVE,
+  TEAM_RECEIVE
 } from './';
 
 // const config : any = yaml.load('../config.yaml');
 
 export default interface TeamPayload {
-  team: number;
-  data: object;
+  team? : number;
+  data : object;
 };
 
-export function selectTeam(team: number) {
+export function selectTeam(team : number) {
   return {
     type: TEAMS_SELECT,
     payload: { team }
   }
 }
 
-export function requestTeam(team: number) : Action<object> {
+export function requestTeam(team : number) : Action<object> {
   return {
-    type: TEAMS_REQUEST,
+    type: TEAM_REQUEST,
     payload: { team },
     meta: {
       requestedAt: Date.now()
@@ -33,9 +35,18 @@ export function requestTeam(team: number) : Action<object> {
   };
 }
 
-export function receiveTeam(team: number, data: object) : Action<object> {
+export function requestTeams() : Action<object> {
   return {
-    type: TEAMS_RECEIVE,
+    type: TEAMS_REQUEST,
+    meta: {
+      requestedAt: Date.now()
+    }
+  };
+}
+
+export function receiveTeam(team : number, data : object) : Action<object> {
+  return {
+    type: TEAM_RECEIVE,
     payload: { team, data },
     meta: {
       receivedAt: Date.now()
@@ -43,14 +54,36 @@ export function receiveTeam(team: number, data: object) : Action<object> {
   };
 }
 
-export function fetchTeam(team: number) : any {
-  let url: string = `http://localhost:8000/teams/${team}/`;
+export function receiveTeams(data : object) : Action<object> {
+  return {
+    type: TEAMS_RECEIVE,
+    payload: { data },
+    meta: {
+      receivedAt: Date.now()
+    }
+  };
+}
+
+export function fetchTeam(team : number) : any {
+  let url : string = `http://localhost:8000/teams/${team}/`;
 
   return (dispatch : Dispatch<object>) => {
     dispatch(requestTeam(team));
 
     return fetch(url)
-    .then((response: any) => response.json())
-    .then((json : object) => dispatch(receiveTeam(team, json)))
+    .then((response : any) => response.json())
+    .then((json : object) => dispatch(receiveTeam(team, json)));
+  };
+}
+
+export function fetchTeams(dispatch : Dispatch<object>) : any {
+  let url : string = 'http://localhost:8000/teams/';
+
+  return (dispatch : Dispatch<object>) => {
+    dispatch(requestTeams());
+
+    return fetch(url)
+    .then((response : any) => response.json())
+    .then((json : object) => dispatch(receiveTeams(json)));
   };
 }
