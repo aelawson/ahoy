e2e86289b256aebcfc457208d9f2f67681ab776e
Andrew Lawson <alawson@jwplayer.com>
2018-04-01 18:31:33 -0400

    Add connected switch to ensure mutex rendering. Add teams/:id route for each teams releases. Other small changes.

--- app/containers/app.tsx
+++ app/containers/app.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import { deepOrange500 } from 'material-ui/styles/colors';
 import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
 
 import NavBar from "./navbar";
-import ReleaseMetadata from "../components/release/metadata";
-import Teams from "../containers/teams";
+import Teams from "./teams";
+import TeamReleases from './team-releases.container';
 
 const styles = require("./main.module.less");
 const muiTheme = getMuiTheme({
@@ -16,13 +16,20 @@ const muiTheme = getMuiTheme({
   },
 });
 
+const ConnectedSwitch = connect<any, any>((state:any) => ({
+  location: state.router.location
+}))(Switch)
+
 class App extends React.Component<any, any> {
   render() {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <NavBar/>
-        <Route path="/teams" component={Teams}/>
-        <Route exact path='/' component={Teams}/>
+        <ConnectedSwitch>
+          <Route path="/teams/:id" component={TeamReleases}/>
+          <Route path="/teams" component={Teams}/>
+          <Route exact path='/' component={Teams}/>
+        </ConnectedSwitch>
       </MuiThemeProvider>
     )
   }
